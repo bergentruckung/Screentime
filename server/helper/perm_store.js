58dd65d84548bd4b85849bb9653597d7b2cf0c0d
Sreeraj <sreerajmylatty@gmail.com>
2016-02-18 15:38:04 +0530

    login and signup db connection done

--- server/helper/perm_store.js
+++ server/helper/perm_store.js
@@ -1,6 +1,9 @@
 var mysql = require('mysql');
 var connection;
 
+var LOGINTABLE = 'login';
+var DEVICETABLE = 'device';
+
 exports.initialize = function(db_host,db_user,db_pass,db_database) {
     connection = mysql.createConnection({
         host: db_host,
@@ -18,23 +21,78 @@ exports.initialize = function(db_host,db_user,db_pass,db_database) {
     });
 };
 
-exports.get_customer_phoneno = function(c_id) {
-    if(c_id == 'sanid123'){
-        return '918289903935';
-    }else if(c_id == 'sreeraj123'){
-        return '918086084339';        
-    }
+exports.signupUser = function(username , email , password) {
+    query = 'insert into '+ LOGINTABLE +' (name,email,password) values ("' + username + '", "' + email + '",  "'+  password +  '")';
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else console.log('success');
+    });
 };
 
-exports.verify_pin = function(c_id, pin) {
-    console.log("cid: " + c_id);
-    console.log("pin: " + pin);
-    if(c_id == 'sanid123' && pin == '44433'){
-       return true;
-    }else if(c_id == 'sreeraj123' && pin == '44444'){
-        return true;
-    }else{
-        return false;
-    }
-    
-}
+exports.addDevice = function(email , deviceID) {
+    query = 'insert into '+ LOGINTABLE +' (name,email,password) values ("' + username + '", "' + email + '",  "'+  password +  '")';
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else console.log('success');
+    });
+};
+
+exports.addDevice = function(emailid , deviceid){
+    query = 'insert into '+ DEVICETABLE +' (email,deviceid) values ("' + emailid  + '",  "'+  deviceid +  '")';
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else console.log('success');
+    });
+};
+
+exports.checkLogin = function(emailid , password , callback){
+    query = 'select count(*) as solution from '+ LOGINTABLE + ' WHERE email = "' + emailid + '" AND PASSWORD = "' + password + '"';
+    console.log(query);
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else {
+            console.log("present : " + results[0].solution);
+            if(results[0].solution == '0'){
+                callback('failed');
+            }else{
+                callback('success');
+            } 
+        }
+    });
+};
+
+exports.checkDeviceRegistration = function(email , deviceid , callback){
+    query = 'select hash as solution from '+ DEVICETABLE + ' WHERE email = "' + email + '" AND deviceid = "' + deviceid + '"';
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else {
+            if(results.length == 0){
+                callback('failed');
+            }else{
+                console.log("presented hash : " + results[0].solution);
+                callback(results[0].solution);
+            }
+        }
+    });
+};
+
+exports.insertDevice = function(email , deviceid ,hash ,  callback){
+
+    query = "insert into " + DEVICETABLE + " values( '" + email + "' , '"+ deviceid +"','" +hash + "')";
+    console.log(query);
+    connection.query(query,
+    function selectCb(err, results, fields) {
+        if (err) throw err;
+        else {
+            callback('success');
+        }
+    });
+};
+
+
+
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var perm_store = require('./helper/perm_store.js');
-
+var hash_code = require('./helper/hash.js');
 
 app.set('views', './views');
 app.use(express.static('./public'));
@@ -25,8 +25,40 @@ app.get('/', function(req, res) {
 });
 
 
-app.post('/api/android/sessiondata/', function(req , res){
-    console.log('session from android');
+app.post('/api/android/signup/', function(req , res){
+    var user_id = req.body.user_id;
+    var user_name = req.body.user_name;
+    var pass = req.body.pass;
+
+    perm_store.signupUser(user_name, user_id , pass);
+  	res.send('done');
+});
+
+app.post('/api/login', function(req, res){
+	var user_id = req.body.user_id;
+	var dev_id = req.body.device_id;
+	var pass = req.body.pass;
+	perm_store.checkLogin(user_id, pass, function(info){
+		if(info == 'failed'){
+			res.send('failed login');
+		}else if(info == "success"){
+			perm_store.checkDeviceRegistration(user_id, dev_id, function(info){
+				if(info == 'failed'){
+					var hashValue = hash_code.getHash(user_id + ":" + dev_id);
+					perm_store.insertDevice(user_id,dev_id,hashValue,function(info){
+						res.send(hashValue);
+					});
+				}
+			});	
+	}
+	});
+	
+});
+
+app.post('/api/device/session/', function(req , res){
+
+    var hash = req.body.hash;
+    var data = req.body.session_data;
 });
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
